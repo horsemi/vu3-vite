0af3eb8e6355d408c738bf5ecd90ec18060301a5
FE <luwenzhuo@linshimuye.com>
2021-03-29 16:06:07 +0800

    添加 vuex 应用

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,16 +1,26 @@
 <template>
   <img alt="Vue logo" src="../../assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld :msg="'userId: ' + userId + ' userName: ' + userName" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '/@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
+import HelloWorld from '/@/components/HelloWorld.vue';
+
+import { userStore } from '/@/store/modules/user';
 
 export default defineComponent({
   name: 'Dashboard',
   components: {
     HelloWorld
+  },
+  setup() {
+    userStore.login();
+    const { userId, userName} = userStore.getUserInfo;
+    return {
+      userId,
+      userName
+    };
   }
 })
 </script>
