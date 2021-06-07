b3d95b4352860b849325f2575244b218ae97f6a2
FE <luwenzhuo@linshimuye.com>
2021-06-07 19:41:50 +0800

    修复 菜单 dxdrawer 打开关闭模式

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -5,7 +5,7 @@
     </div>
     <DxDrawer
       v-model:opened="openState"
-      opened-state-mode="push"
+      opened-state-mode="shrink"
       reveal-mode="expand"
       class="layout-body"
       position="before"
@@ -37,7 +37,7 @@
         </div>
         <div class="content-wrap">
           <MultipleTabs />
-          <DxScrollView>
+          <DxScrollView class="with-footer">
             <LayoutContent />
           </DxScrollView>
         </div>
@@ -50,99 +50,111 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, ref } from 'vue';
-import LayoutContent from './default/content/index.vue';
-import LayoutHeader from './default/header/index.vue';
-import LayoutMenu from './default/menu/index.vue';
-import MultipleTabs from './default/tabs/index.vue';
+  import { defineComponent, computed, ref } from 'vue';
+  import LayoutContent from './default/content/index.vue';
+  import LayoutHeader from './default/header/index.vue';
+  import LayoutMenu from './default/menu/index.vue';
+  import MultipleTabs from './default/tabs/index.vue';
 
-import DxDrawer from 'devextreme-vue/drawer';
-import DxScrollView from 'devextreme-vue/scroll-view';
-import { useViewStore } from '/@/store/modules/view';
-import { useDesign } from '/@/hooks/web/useDesign';
+  import DxDrawer from 'devextreme-vue/drawer';
+  import DxScrollView from 'devextreme-vue/scroll-view';
+  import { useViewStore } from '/@/store/modules/view';
+  import { useDesign } from '/@/hooks/web/useDesign';
 
-export default defineComponent({
-  name: 'Layout',
-  components: {
-    LayoutContent,
-    LayoutHeader,
-    LayoutMenu,
-    MultipleTabs,
-    DxDrawer,
-    DxScrollView,
-  },
-  setup() {
-    const menuSize = {
-      max: 200,
-      min: 54
-    };
-    const viewStore = useViewStore();
-    const prefixCls = useDesign('layout');
-    const viewState = computed(() => viewStore.getViewList);
-    const openState = ref(true);
-    const toggleMenu = () => {
-      openState.value = !openState.value;
-    };
+  export default defineComponent({
+    name: 'Layout',
+    components: {
+      LayoutContent,
+      LayoutHeader,
+      LayoutMenu,
+      MultipleTabs,
+      DxDrawer,
+      DxScrollView,
+    },
+    setup() {
+      const menuSize = {
+        max: 200,
+        min: 54,
+      };
+      const viewStore = useViewStore();
+      const prefixCls = useDesign('layout');
+      const viewState = computed(() => viewStore.getViewList);
+      const openState = ref(true);
+      const toggleMenu = () => {
+        openState.value = !openState.value;
+      };
 
-    return {
-      viewState,
-      openState,
-      prefixCls,
-      toggleMenu,
-      menuSize
-    };
-  },
-});
+      return {
+        viewState,
+        openState,
+        prefixCls,
+        toggleMenu,
+        menuSize,
+      };
+    },
+  });
 </script>
 
 <style lang="less" scoped>
-@prefix-cls: ~'@{namespace}-layout';
+  @prefix-cls: ~'@{namespace}-layout';
 
-.@{prefix-cls} {
-  flex-direction: column;
-  display: flex;
-  width: 100%;
-  height: 100%;
-
-  &__box {
+  .@{prefix-cls} {
+    flex-direction: column;
     display: flex;
-  }
-}
+    width: 100%;
+    height: 100%;
 
-.layout-body {
-  flex: 1;
-  min-height: 0;
-
-  .layout-body-content {
-    display: flex;
-
-    .menu-button {
+    &__box {
       display: flex;
-      align-items: center;
-      width: 20px;
-      height: calc(100vh - 50px);
-      background: @background-color-primary;
-
-      .menu-button-box {
-        width: 12px;
-        height: 80px;
-        line-height: 80px;
-        cursor: pointer;
-        background: #fff;
-        border-radius: 0 20px 20px 0;
-      }
-
-      .menu-button-icon {
-        position: relative;
-        left: -3px;
-        margin: 0;
-        vertical-align: middle;
-      }
-    }
-
-    .content-wrap {
-      flex: 1;
     }
   }
-}
+
+  .layout-body {
+    flex: 1;
+    min-height: 0;
+
+    .layout-body-content {
+      display: flex;
+
+      .menu-button {
+        display: flex;
+        align-items: center;
+        width: 20px;
+        height: calc(100vh - 50px);
+        background: @background-color-primary;
+
+        .menu-button-box {
+          width: 12px;
+          height: 80px;
+          line-height: 80px;
+          cursor: pointer;
+          background: #fff;
+          border-radius: 0 20px 20px 0;
+        }
+
+        .menu-button-icon {
+          position: relative;
+          left: -3px;
+          margin: 0;
+          vertical-align: middle;
+        }
+      }
+
+      .content-wrap {
+        flex: 1;
+      }
+    }
+  }
+</style>
+
+<style lang="less">
+  .with-footer > .dx-scrollable-wrapper > .dx-scrollable-container > .dx-scrollable-content {
+    height: 100%;
+
+    & > .dx-scrollview-content {
+      display: flex;
+      flex-direction: column;
+      min-height: 100%;
+    }
+  }
 </style>
