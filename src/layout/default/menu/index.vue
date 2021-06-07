57e347d1d660c12e5c45bc6d55987bfb7fd021fa
吕振业 <lvzhenye@linshimuye.com>
2021-06-07 16:45:24 +0800

    优化menu动画效果

--- src/layout/default/menu/index.vue
+++ src/layout/default/menu/index.vue
@@ -1,355 +1,326 @@
 <template>
-  <div>
-    <div :class="[prefixCls]">
-      <div
-        v-for="(item, index) in menuData"
-        :key="index"
-        :class="[`${prefixCls}__sub`, activeIndex === index && `${prefixCls}__sub--active`]"
-        @click.stop="itemClick(item, index)"
-      >
-        <SvgIcon size="16" name="sun"></SvgIcon>
-        <span :class="`${prefixCls}__sub__title`">{{ item.title }}</span>
-        <transition name="item-box">
-          <div v-show="item.show" :class="`${prefixCls}__item`" :style="{ top: subTop + 'px' }">
-            <transition name="title-fade">
-              <div v-show="item.show" :class="`${prefixCls}__titleWrap`">
-                <div
-                  v-for="(itemSub, indxeSub) in item.children"
-                  :key="indxeSub"
-                  :class="`${prefixCls}__titlebox`"
-                >
-                  <div :class="`${prefixCls}__titlebox__subTitle`">{{ itemSub.title }}</div>
-                  <div
-                    v-for="(subChild, indexChild) in itemSub.children"
-                    :key="indexChild"
-                    :class="`${prefixCls}__titlebox__childTitle`"
-                  >
-                    {{ subChild.title }}
-                  </div>
-                </div>
+  <div :class="[prefixCls]">
+    <div
+      v-for="(item, index) in menuData"
+      :key="index"
+      :class="[`${prefixCls}__sub`, activeIndex === index && `${prefixCls}__sub--active`]"
+      @click.stop="itemClick(item, index)"
+    >
+      <SvgIcon size="16" name="sun"></SvgIcon>
+      <span v-show="openState" :class="`${prefixCls}__sub__title`">{{ item.title }}</span>
+      <transition name="zoom-in-left">
+        <div
+          v-show="item.show"
+          :class="`${prefixCls}__item`"
+          :style="{ top: (index + 1) * 50 + 10 + 'px', left: openState ? menuSize.max +'px' : menuSize.min +'px' }"
+        >
+          <div :class="`${prefixCls}__titlewrap`">
+            <div
+              v-for="(itemSub, indxeSub) in item.children"
+              :key="indxeSub"
+              :class="`${prefixCls}__titlebox`"
+            >
+              <div :class="`${prefixCls}__titlebox__subTitle`">{{ itemSub.title }}</div>
+              <div
+                v-for="(subChild, indexChild) in itemSub.children"
+                :key="indexChild"
+                :class="`${prefixCls}__titlebox__childTitle`"
+              >
+                {{ subChild.title }}
               </div>
-            </transition>
+            </div>
           </div>
-        </transition>
-      </div>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import { useGo } from '/@/hooks/web/usePage';
-  import { useDesign } from '/@/hooks/web/useDesign';
+import { defineComponent, ref } from 'vue';
+import { useGo } from '/@/hooks/web/usePage';
+import { useDesign } from '/@/hooks/web/useDesign';
 
-  export default defineComponent({
-    name: 'LayoutMenu',
-    setup() {
-      const go = useGo();
-      const headerHeight = 50;
-      const subTop = ref(headerHeight);
-      const activeIndex = ref(0);
-      const menuData = ref([
-        {
-          title: '系统首页',
-          show: false,
-        },
-        {
-          title: '订单管理',
-          show: false,
-          children: [
-            {
-              title: '发货管理',
-              children: [
-                {
-                  title: '发货单',
-                },
-                {
-                  title: '发货通知单',
-                },
-              ],
-            },
-            {
-              title: '出库管理',
-              children: [
-                {
-                  title: '出库单',
-                },
-                {
-                  title: '出库通知单',
-                },
-              ],
-            },
-            {
-              title: '入库管理',
-              children: [
-                {
-                  title: '入库单',
-                },
-                {
-                  title: '入库通知单',
-                },
-              ],
-            },
-            {
-              title: '库内管理',
-              children: [
-                {
-                  title: '直接调拨单',
-                },
-                {
-                  title: '组装拆卸单',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          title: '调度计划',
-          show: false,
-          children: [
-            {
-              title: '波次计划',
-              children: [
-                {
-                  title: '发货波次',
-                },
-                {
-                  title: '波次看板',
-                },
-              ],
-            },
-            {
-              title: '排班计划',
-              children: [
-                {
-                  title: '排班记录',
-                },
-                {
-                  title: '排班看板',
-                },
-              ],
-            },
-            {
-              title: '约车计划',
-              children: [
-                {
-                  title: '约车记录',
-                },
-                {
-                  title: '约车看板',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          title: '库存中心',
-          show: false,
-          children: [
-            {
-              title: '库存查询',
-              children: [
-                {
-                  title: '库存查询',
-                },
-                {
-                  title: '库存收发明细',
-                },
-              ],
-            },
-            {
-              title: '库存占用',
-              children: [
-                {
-                  title: '占用日志查询',
-                },
-              ],
-            },
-            {
-              title: '其他配置',
-              children: [
-                {
-                  title: '仓库分配优先级',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          title: '基础管理',
-          show: false,
-          children: [
-            {
-              title: '基础数据',
-              children: [
-                {
-                  title: '物料',
-                },
-                {
-                  title: '物料清单',
-                },
-                {
-                  title: '锁单路线',
-                },
-              ],
-            },
-            {
-              title: '系统配置',
-              children: [
-                {
-                  title: '清除缓存',
-                },
-              ],
-            },
-          ],
-        },
-      ]);
-
-      function handleItemClick(e) {
-        if (!e.itemData.items) {
-          go(e.itemData.path);
-        }
+export default defineComponent({
+  name: 'LayoutMenu',
+  props: {
+    openState: Boolean,
+    menuSize: {
+      type: Object,
+      default: () =>{
+        return {};
       }
+    }
+  },
+  emits: ['toggle-menu'],
+  setup() {
+    const go = useGo();
+    const activeIndex = ref(0);
+    const menuData = ref([
+      {
+        title: '系统首页',
+        show: false,
+      },
+      {
+        title: '订单管理',
+        show: false,
+        children: [
+          {
+            title: '发货管理',
+            children: [
+              {
+                title: '发货单',
+              },
+              {
+                title: '发货通知单',
+              },
+            ],
+          },
+          {
+            title: '出库管理',
+            children: [
+              {
+                title: '出库单',
+              },
+              {
+                title: '出库通知单',
+              },
+            ],
+          },
+          {
+            title: '入库管理',
+            children: [
+              {
+                title: '入库单',
+              },
+              {
+                title: '入库通知单',
+              },
+            ],
+          },
+          {
+            title: '库内管理',
+            children: [
+              {
+                title: '直接调拨单',
+              },
+              {
+                title: '组装拆卸单',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        title: '调度计划',
+        show: false,
+        children: [
+          {
+            title: '波次计划',
+            children: [
+              {
+                title: '发货波次',
+              },
+              {
+                title: '波次看板',
+              },
+            ],
+          },
+          {
+            title: '排班计划',
+            children: [
+              {
+                title: '排班记录',
+              },
+              {
+                title: '排班看板',
+              },
+            ],
+          },
+          {
+            title: '约车计划',
+            children: [
+              {
+                title: '约车记录',
+              },
+              {
+                title: '约车看板',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        title: '库存中心',
+        show: false,
+        children: [
+          {
+            title: '库存查询',
+            children: [
+              {
+                title: '库存查询',
+              },
+              {
+                title: '库存收发明细',
+              },
+            ],
+          },
+          {
+            title: '库存占用',
+            children: [
+              {
+                title: '占用日志查询',
+              },
+            ],
+          },
+          {
+            title: '其他配置',
+            children: [
+              {
+                title: '仓库分配优先级',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        title: '基础管理',
+        show: false,
+        children: [
+          {
+            title: '基础数据',
+            children: [
+              {
+                title: '物料',
+              },
+              {
+                title: '物料清单',
+              },
+              {
+                title: '锁单路线',
+              },
+            ],
+          },
+          {
+            title: '系统配置',
+            children: [
+              {
+                title: '清除缓存',
+              },
+            ],
+          },
+        ],
+      },
+    ]);
 
-      function itemClick(item, index) {
+    function handleItemClick(e) {
+      if (!e.itemData.items) {
+        go(e.itemData.path);
+      }
+    }
+
+    function itemClick(item, index) {
+      menuData.value[activeIndex.value].show = false;
+      activeIndex.value = index;
+      if (item.children) {
+        item.show = true;
+      }
+    }
+
+    document.body.addEventListener(
+      'click',
+      () => {
         menuData.value[activeIndex.value].show = false;
-        activeIndex.value = index;
-        if (item.children) {
-          subTop.value = headerHeight * (index + 1);
-          item.show = true;
-        }
-      }
+      },
+      false
+    );
 
-      document.body.addEventListener(
-        'click',
-        () => {
-          menuData.value[activeIndex.value].show = false;
-        },
-        false
-      );
+    const { prefixCls } = useDesign('layout-menu');
 
-      const { prefixCls } = useDesign('layout-menu');
-
-      return {
-        menuData,
-        handleItemClick,
-        prefixCls,
-        itemClick,
-        subTop,
-        activeIndex,
-      };
-    },
-  });
+    return {
+      menuData,
+      handleItemClick,
+      prefixCls,
+      itemClick,
+      activeIndex,
+    };
+  },
+});
 </script>
 
 <style lang="less" scoped>
-  @prefix-cls: ~'@{namespace}-layout-menu';
+@prefix-cls: ~'@{namespace}-layout-menu';
 
-  .@{prefix-cls} {
-    width: 200px;
-    height: 100%;
+.@{prefix-cls} {
+  width: 200px;
+  height: 100%;
+  padding-top: 10px;
 
-    &__sub {
-      display: flex;
-      width: 100%;
+  &__sub {
+    display: flex;
+    width: 100%;
+    height: 50px;
+    padding: 0 20px;
+    font-size: 16px;
+    color: #000;
+    box-sizing: border-box;
+    align-items: center;
+    .zoom-animation(left, scale(0.45, 0.45), scale(1, 1), top left);
+  }
+
+  &__sub__title {
+    margin-left: 10px;
+    letter-spacing: 1px;
+  }
+
+  &__sub--active {
+    color: @color-primary;
+    background: rgb(230, 247, 255);
+    &::before {
+      position: absolute;
+      left: 0;
+      display: inline-block;
+      width: 6px;
       height: 50px;
-      padding: 0 20px;
-      font-size: 16px;
-      color: #000;
-      box-sizing: border-box;
-      align-items: center;
+      background-color: @color-primary;
+      content: '';
     }
+  }
 
-    &__sub__title {
-      margin-left: 10px;
-      letter-spacing: 1px;
-    }
+  &__item {
+    position: fixed;
+    top: 50px;
+    left: 203px;
+    z-index: @page-layout-menu-z-index;
+    width: 500px;
+    padding: 10px 15px;
+    color: #fff;
+    background: @color-primary;
+    border-radius: 4px;
+  }
 
-    &__sub--active {
-      color: @color-primary;
-      background: rgb(230, 247, 255);
-      &::before {
-        position: absolute;
-        left: 0;
-        display: inline-block;
-        width: 6px;
-        height: 50px;
-        background-color: @color-primary;
-        content: '';
-      }
-    }
+  &__titlewrap {
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-    &__item {
-      position: fixed;
-      top: 50px;
-      left: 203px;
-      z-index: 100;
-      width: 500px;
-      height: 400px;
-      padding: 10px 15px;
-      color: #fff;
-      background: @color-primary;
-      border-radius: 4px;
-      animation: slideOutLeft 1s;
-    }
+  &__titlebox {
+    width: 33.33%;
+    padding: 10px 15px;
+    overflow: hidden;
+    white-space: nowrap;
+    box-sizing: border-box;
 
-    &__titleWrap {
-      display: flex;
+    &__subTitle {
       width: 100%;
-      height: 100%;
-      flex-wrap: wrap;
+      margin-bottom: 10px;
+      color: rgb(198, 224, 209);
     }
 
-    &__titlebox {
-      width: 33.33%;
-      padding: 10px 15px;
-      overflow: hidden;
-      white-space: nowrap;
-      box-sizing: border-box;
-
-      &__subTitle {
-        width: 100%;
-        margin-bottom: 10px;
-        color: rgb(198, 224, 209);
-      }
-
-      &__childTitle {
-        width: 100%;
-        margin-bottom: 5px;
-      }
-    }
-
-    .title-fade-enter-active,
-    .fade-leave-active {
-      transition: opacity 1s;
-    }
-    .title-fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-      opacity: 0;
-    }
-
-    .item-box-enter,
-    .item-box-leave-to {
-      width: 0;
-    }
-    .item-box-leave,
-    .item-box-enter-to {
-      width: 200px;
-    }
-    .item-box-enter-active,
-    .item-box-leave-active {
-      transition: width 0.3s;
+    &__childTitle {
+      width: 100%;
+      margin-bottom: 5px;
     }
   }
-</style>
-
-<style>
-  @keyframes slideOutLeft {
-    0% {
-      width: 0;
-    }
-
-    100% {
-      width: 200px;
-    }
-  }
+}
 </style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,67 +3,146 @@
     <div>
       <LayoutHeader />
     </div>
-    <DxDrawer v-model:opened="openState" class="layout-body" position="before" template="menulist">
-      <MultipleTabs />
-      <DxScrollView>
-        <LayoutContent />
-      </DxScrollView>
+    <DxDrawer
+      v-model:opened="openState"
+      opened-state-mode="push"
+      reveal-mode="expand"
+      class="layout-body"
+      position="before"
+      template="menulist"
+      :max-size="menuSize.max"
+      :min-size="menuSize.min"
+    >
+      <div class="layout-body-content">
+        <div class="menu-button">
+          <div class="menu-button-box" @click="toggleMenu">
+            <svg-icon
+              v-if="opened"
+              name="sidebar-arrow"
+              width="15px"
+              height="15px"
+              color="#808080"
+              class="menu-button-icon"
+            />
+            <svg-icon
+              v-else
+              name="sidebar-arrow"
+              dir="down"
+              width="15px"
+              height="15px"
+              color="#808080"
+              class="menu-button-icon"
+            />
+          </div>
+        </div>
+        <div class="content-wrap">
+          <MultipleTabs />
+          <DxScrollView>
+            <LayoutContent />
+          </DxScrollView>
+        </div>
+      </div>
       <template #menulist>
-        <LayoutMenu />
+        <LayoutMenu :open-state="openState" :menu-size="menuSize" @toggle-menu="toggleMenu" />
       </template>
     </DxDrawer>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent, computed, ref } from 'vue';
-  import LayoutContent from './default/content/index.vue';
-  import LayoutHeader from './default/header/index.vue';
-  import LayoutMenu from './default/menu/index.vue';
-  import MultipleTabs from './default/tabs/index.vue';
+import { defineComponent, computed, ref } from 'vue';
+import LayoutContent from './default/content/index.vue';
+import LayoutHeader from './default/header/index.vue';
+import LayoutMenu from './default/menu/index.vue';
+import MultipleTabs from './default/tabs/index.vue';
 
-  import DxDrawer from 'devextreme-vue/drawer';
-  import DxScrollView from 'devextreme-vue/scroll-view';
-  import { useViewStore } from '/@/store/modules/view';
-  import { useDesign } from '/@/hooks/web/useDesign';
+import DxDrawer from 'devextreme-vue/drawer';
+import DxScrollView from 'devextreme-vue/scroll-view';
+import { useViewStore } from '/@/store/modules/view';
+import { useDesign } from '/@/hooks/web/useDesign';
 
-  export default defineComponent({
-    name: 'Layout',
-    components: {
-      LayoutContent,
-      LayoutHeader,
-      LayoutMenu,
-      MultipleTabs,
-      DxDrawer,
-      DxScrollView,
-    },
-    setup() {
-      const viewStore = useViewStore();
-      const prefixCls = useDesign('layout');
-      const viewState = computed(() => viewStore.getViewList);
-      const openState = ref(true);
+export default defineComponent({
+  name: 'Layout',
+  components: {
+    LayoutContent,
+    LayoutHeader,
+    LayoutMenu,
+    MultipleTabs,
+    DxDrawer,
+    DxScrollView,
+  },
+  setup() {
+    const menuSize = {
+      max: 200,
+      min: 54
+    };
+    const viewStore = useViewStore();
+    const prefixCls = useDesign('layout');
+    const viewState = computed(() => viewStore.getViewList);
+    const openState = ref(true);
+    const toggleMenu = () => {
+      openState.value = !openState.value;
+    };
 
-      return {
-        viewState,
-        openState,
-        prefixCls,
-      };
-    },
-  });
+    return {
+      viewState,
+      openState,
+      prefixCls,
+      toggleMenu,
+      menuSize
+    };
+  },
+});
 </script>
 
 <style lang="less" scoped>
-  @prefix-cls: ~'@{namespace}-layout';
+@prefix-cls: ~'@{namespace}-layout';
 
-  .@{prefix-cls} {
-    flex-direction: column;
+.@{prefix-cls} {
+  flex-direction: column;
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  &__box {
     display: flex;
-    width: 100%;
-    height: 100%;
   }
+}
 
-  .layout-body {
-    flex: 1;
-    min-height: 0;
+.layout-body {
+  flex: 1;
+  min-height: 0;
+
+  .layout-body-content {
+    display: flex;
+
+    .menu-button {
+      display: flex;
+      align-items: center;
+      width: 20px;
+      height: calc(100vh - 50px);
+      background: @background-color-primary;
+
+      .menu-button-box {
+        width: 12px;
+        height: 80px;
+        line-height: 80px;
+        cursor: pointer;
+        background: #fff;
+        border-radius: 0 20px 20px 0;
+      }
+
+      .menu-button-icon {
+        position: relative;
+        left: -3px;
+        margin: 0;
+        vertical-align: middle;
+      }
+    }
+
+    .content-wrap {
+      flex: 1;
+    }
   }
+}
 </style>
