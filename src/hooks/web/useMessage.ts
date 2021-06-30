59631ced52487230ac747c58d76d63795026b797
FE <luwenzhuo@linshimuye.com>
2021-06-30 09:04:49 +0800

    添加全局信息管理 模块 useMessage

--- src/hooks/web/useMessage.ts
+++ src/hooks/web/useMessage.ts
@@ -1,3 +1,56 @@
+import notify from 'devextreme/ui/notify';
+
+type MessageType = 'success' | 'warning' | 'error' | 'info';
+
 export function errorMessage(msg: string | Error) {
-  console.error(msg);
+  notify(
+    { message: msg, width: 300, position: { at: 'top center', my: 'top center', offset: '0 50' } },
+    'error',
+    1000
+  );
+}
+
+export function successMessage(msg: string) {
+  notify(
+    { message: msg, width: 300, position: { at: 'top center', my: 'top center', offset: '0 50' } },
+    'success',
+    1000
+  );
+}
+
+export function warningMessage(msg: string) {
+  notify(
+    { message: msg, width: 300, position: { at: 'top center', my: 'top center', offset: '0 50' } },
+    'warning',
+    1000
+  );
+}
+
+export function infoMessage(msg: string) {
+  notify(
+    { message: msg, width: 300, position: { at: 'top center', my: 'top center', offset: '0 50' } },
+    'info',
+    1000
+  );
+}
+
+export function useMessage(message: string | Error, type: MessageType) {
+  switch (type) {
+    case 'error': {
+      errorMessage(message);
+      break;
+    }
+    case 'warning': {
+      warningMessage(message as string);
+      break;
+    }
+    case 'success': {
+      successMessage(message as string);
+      break;
+    }
+    case 'info': {
+      infoMessage(message as string);
+      break;
+    }
+  }
 }
