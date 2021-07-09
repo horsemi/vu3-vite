5d0f1c486b975e2a7b2dc36235bc1c7d8f5cd7d4
FE <luwenzhuo@linshimuye.com>
2021-07-09 10:20:04 +0800

    详情页 添加component name

--- src/views/example/details.vue
+++ src/views/example/details.vue
@@ -70,239 +70,240 @@
 </template>
 
 <script lang="ts">
-import type { ITableOptions } from '/@/components/Table/types';
-import type { IDetailItem } from '/@/utils/detail/types';
+  import type { ITableOptions } from '/@/components/Table/types';
+  import type { IDetailItem } from '/@/utils/detail/types';
 
-import { defineComponent, nextTick, onMounted, ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-import { cloneDeep } from 'lodash-es';
+  import { defineComponent, nextTick, onMounted, ref, watch } from 'vue';
+  import { useRoute } from 'vue-router';
+  import { cloneDeep } from 'lodash-es';
 
-import { defaultTableOptions } from '/@/components/Table/common';
-import { deepMerge } from '/@/utils';
-import { ShippingOrderApi } from '/@/api/ods/shipping-orders';
-import { getDetailData, getDefiniteData, customDefinite } from './index';
+  import { defaultTableOptions } from '/@/components/Table/common';
+  import { deepMerge } from '/@/utils';
+  import { ShippingOrderApi } from '/@/api/ods/shipping-orders';
+  import { getDetailData, getDefiniteData, customDefinite } from './index';
 
-import DxTabPanel from 'devextreme-vue/tab-panel';
-import DxButton from 'devextreme-vue/button';
+  import DxTabPanel from 'devextreme-vue/tab-panel';
+  import DxButton from 'devextreme-vue/button';
 
-import DetailForm from '/@/components/DetailForm/index.vue';
+  import DetailForm from '/@/components/DetailForm/index.vue';
 
-export default defineComponent({
-  components: {
-    DxTabPanel,
-    DxButton,
-    DetailForm,
-  },
-  setup() {
-    const multiViewItems = ref([
-      {
-        title: '基本信息',
-        height: '',
-      },
-      {
-        title: '收货人信息',
-        height: '',
-      },
-      {
-        title: '物流信息',
-        height: '',
-      },
-      {
-        title: '其他信息',
-        height: '',
-      },
-    ]);
-    const multiEntityItems = [
-      {
-        title: '明细信息',
-      },
-    ];
+  export default defineComponent({
+    name: 'ShippingOrderDetail',
+    components: {
+      DxTabPanel,
+      DxButton,
+      DetailForm,
+    },
+    setup() {
+      const multiViewItems = ref([
+        {
+          title: '基本信息',
+          height: '',
+        },
+        {
+          title: '收货人信息',
+          height: '',
+        },
+        {
+          title: '物流信息',
+          height: '',
+        },
+        {
+          title: '其他信息',
+          height: '',
+        },
+      ]);
+      const multiEntityItems = [
+        {
+          title: '明细信息',
+        },
+      ];
 
-    const opened = ref(true);
-    const selectedIndex = ref(0);
-    const formBox = ref();
-    const tableOpenedHeight = ref('');
-    const tableCloseHeight = 'calc(100vh - 28px - 244px)';
+      const opened = ref(true);
+      const selectedIndex = ref(0);
+      const formBox = ref();
+      const tableOpenedHeight = ref('');
+      const tableCloseHeight = 'calc(100vh - 28px - 244px)';
 
-    const route = useRoute();
-    const Id = parseInt(route.query.Id as string);
-    const formData = ref();
-    const baseInformation = ref<IDetailItem[]>([]);
-    const consigneeInformation = ref<IDetailItem[]>([]);
-    const logisticsInformation = ref<IDetailItem[]>([]);
-    const otherInformation = ref<IDetailItem[]>([]);
+      const route = useRoute();
+      const Id = parseInt(route.query.Id as string);
+      const formData = ref();
+      const baseInformation = ref<IDetailItem[]>([]);
+      const consigneeInformation = ref<IDetailItem[]>([]);
+      const logisticsInformation = ref<IDetailItem[]>([]);
+      const otherInformation = ref<IDetailItem[]>([]);
 
-    const options: Partial<ITableOptions> = {
-      useScrolling: true,
-      showBorders: false,
-      page: {
-        size: 10,
-      },
-    };
-    const tableOptions = ref<ITableOptions>(deepMerge(cloneDeep(defaultTableOptions), options));
-    const dataSource = ref();
-    const columns = customDefinite;
+      const options: Partial<ITableOptions> = {
+        useScrolling: true,
+        showBorders: false,
+        page: {
+          size: 10,
+        },
+      };
+      const tableOptions = ref<ITableOptions>(deepMerge(cloneDeep(defaultTableOptions), options));
+      const dataSource = ref();
+      const columns = customDefinite;
 
-    const onRefresh = () => {
-      getData();
-    };
+      const onRefresh = () => {
+        getData();
+      };
 
-    const onSubmitClick = () => {
-      ShippingOrderApi.onShippingOrderSubmit([formData.value.GatheringParentCode]).then(() => {
-        onRefresh();
+      const onSubmitClick = () => {
+        ShippingOrderApi.onShippingOrderSubmit([formData.value.GatheringParentCode]).then(() => {
+          onRefresh();
+        });
+      };
+
+      const onApplyClick = () => {
+        ShippingOrderApi.onShippingOrderApply([formData.value.GatheringParentCode]).then(() => {
+          onRefresh();
+        });
+      };
+
+      const handleHeight = (index: number) => {
+        nextTick(() => {
+          if (!multiViewItems.value[index].height) {
+            multiViewItems.value[index].height = formBox.value.offsetHeight + 'px';
+          }
+          tableOpenedHeight.value = `calc(100vh - ${multiViewItems.value[index].height} - 244px)`;
+        });
+      };
+
+      const getData = async () => {
+        getDefiniteData(tableOptions.value, ['ShippingOrderId', '=', Id]).then((res) => {
+          dataSource.value = res;
+        });
+        const detailData = await getDetailData(['Id', '=', Id]);
+        if (!detailData) return;
+        const { baseList, consigneeList, logisticsList, otherList, data } = detailData;
+        formData.value = data;
+        baseInformation.value = baseList;
+        consigneeInformation.value = consigneeList;
+        logisticsInformation.value = logisticsList;
+        otherInformation.value = otherList;
+        handleHeight(0);
+      };
+
+      watch(selectedIndex, (val) => {
+        handleHeight(val);
       });
-    };
 
-    const onApplyClick = () => {
-      ShippingOrderApi.onShippingOrderApply([formData.value.GatheringParentCode]).then(() => {
-        onRefresh();
+      onMounted(async () => {
+        await getData();
       });
-    };
 
-    const handleHeight = (index: number) => {
-      nextTick(() => {
-        if (!multiViewItems.value[index].height) {
-          multiViewItems.value[index].height = formBox.value.offsetHeight + 'px';
-        }
-        tableOpenedHeight.value = `calc(100vh - ${multiViewItems.value[index].height} - 244px)`;
-      });
-    };
-
-    const getData = async () => {
-      getDefiniteData(tableOptions.value, ['ShippingOrderId', '=', Id]).then(res => {
-        dataSource.value = res;
-      });
-      const detailData = await getDetailData(['Id', '=', Id]);
-      if (!detailData) return;
-      const { baseList, consigneeList, logisticsList, otherList, data } = detailData;
-      formData.value = data;
-      baseInformation.value = baseList;
-      consigneeInformation.value = consigneeList;
-      logisticsInformation.value = logisticsList;
-      otherInformation.value = otherList;
-      handleHeight(0);
-    };
-
-    watch(selectedIndex, (val) => {
-      handleHeight(val);
-    });
-
-    onMounted(async () => {
-      await getData();
-    });
-
-    return {
-      baseInformation,
-      consigneeInformation,
-      logisticsInformation,
-      otherInformation,
-      columns,
-      selectedIndex,
-      formBox,
-      opened,
-      multiViewItems,
-      multiEntityItems,
-      formData,
-      dataSource,
-      tableOptions,
-      tableOpenedHeight,
-      tableCloseHeight,
-      onSubmitClick,
-      onApplyClick,
-      onRefresh,
-    };
-  },
-});
+      return {
+        baseInformation,
+        consigneeInformation,
+        logisticsInformation,
+        otherInformation,
+        columns,
+        selectedIndex,
+        formBox,
+        opened,
+        multiViewItems,
+        multiEntityItems,
+        formData,
+        dataSource,
+        tableOptions,
+        tableOpenedHeight,
+        tableCloseHeight,
+        onSubmitClick,
+        onApplyClick,
+        onRefresh,
+      };
+    },
+  });
 </script>
 
 <style lang="less">
-.detail {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-
-  .tab-panel {
-    position: relative;
+  .detail {
+    flex: 1;
     display: flex;
     flex-direction: column;
-    background-color: #fff;
-    &:last-child {
-      flex: 1;
-      margin-top: 10px;
-    }
-  }
-
-  .tab {
-    padding: 8px 20px;
-    background-color: #fff;
-    &:last-child {
-      transition: height 500ms;
-    }
-  }
-  .btn-box {
-    position: absolute;
-    top: 0;
-    right: 20px;
-    z-index: 100;
-    display: flex;
-    align-items: center;
-    height: 36px;
-    & > * {
-      margin-left: 10px;
-    }
-  }
-  .form-box {
-    padding: 0 20px;
     overflow: hidden;
-    transition: height 500ms;
-  }
-  .icon-box {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    padding-top: 8px;
-    .icon {
-      cursor: pointer;
-      transform: rotate(0);
-      transition: transform 500ms;
-      &--translate {
-        transform: rotate(-180deg);
-        transition: transform 500ms;
+
+    .tab-panel {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      background-color: #fff;
+      &:last-child {
+        flex: 1;
+        margin-top: 10px;
       }
     }
-  }
 
-  .dx-layout-manager .dx-field-item:not(.dx-first-row) {
-    padding-top: 8px !important;
-  }
+    .tab {
+      padding: 8px 20px;
+      background-color: #fff;
+      &:last-child {
+        transition: height 500ms;
+      }
+    }
+    .btn-box {
+      position: absolute;
+      top: 0;
+      right: 20px;
+      z-index: 100;
+      display: flex;
+      align-items: center;
+      height: 36px;
+      & > * {
+        margin-left: 10px;
+      }
+    }
+    .form-box {
+      padding: 0 20px;
+      overflow: hidden;
+      transition: height 500ms;
+    }
+    .icon-box {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      padding-top: 8px;
+      .icon {
+        cursor: pointer;
+        transform: rotate(0);
+        transition: transform 500ms;
+        &--translate {
+          transform: rotate(-180deg);
+          transition: transform 500ms;
+        }
+      }
+    }
 
-  .dx-widget {
-    font-size: 12px !important;
-  }
+    .dx-layout-manager .dx-field-item:not(.dx-first-row) {
+      padding-top: 8px !important;
+    }
 
-  .dx-box-item-content {
-    font-size: 12px !important;
-  }
+    .dx-widget {
+      font-size: 12px !important;
+    }
 
-  .dx-texteditor-input {
-    min-height: 0 !important;
-    padding: 5px 9px 5px !important;
-  }
+    .dx-box-item-content {
+      font-size: 12px !important;
+    }
 
-  .dx-button-has-text .dx-button-content {
-    padding: 0 !important;
-  }
+    .dx-texteditor-input {
+      min-height: 0 !important;
+      padding: 5px 9px 5px !important;
+    }
 
-  .dx-layout-manager .dx-label-h-align .dx-field-item-content .dx-checkbox,
-  .dx-layout-manager .dx-label-h-align .dx-field-item-content .dx-switch {
-    margin: 0 !important;
-  }
+    .dx-button-has-text .dx-button-content {
+      padding: 0 !important;
+    }
 
-  .dx-datagrid-table .dx-freespace-row > td {
-    // 去掉空余空间的边框，当指定表格高度时，会出现这个占满空余空间
-    border: none !important;
+    .dx-layout-manager .dx-label-h-align .dx-field-item-content .dx-checkbox,
+    .dx-layout-manager .dx-label-h-align .dx-field-item-content .dx-switch {
+      margin: 0 !important;
+    }
+
+    .dx-datagrid-table .dx-freespace-row > td {
+      // 去掉空余空间的边框，当指定表格高度时，会出现这个占满空余空间
+      border: none !important;
+    }
   }
-}
 </style>
