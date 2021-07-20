d2b22e151faa0fb59afb3e8631c526cb440c678b
FE <luwenzhuo@linshimuye.com>
2021-07-20 11:01:35 +0800

    去除多余路由守卫代码

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -1,12 +1,9 @@
 import router from '/@/router';
 
-import { createPageGuard } from './pageGuard';
 import { createPermissionGuard } from './permissionGuard';
 
 import { createPageUuidGuard } from './pageUuidGuard';
 
 createPageUuidGuard(router);
-
-createPageGuard(router);
 
 createPermissionGuard(router);
--- src/router/guard/pageGuard.ts
+++
@@ -1,7 +0,0 @@
-import type { Router } from 'vue-router';
-
-export function createPageGuard(router: Router) {
-  router.beforeEach(async (to) => {
-    return true;
-  });
-}
