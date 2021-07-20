6a81a201c166f0eb7f6b99732d400d095276bbf9
FE <luwenzhuo@linshimuye.com>
2021-07-20 20:49:26 +0800

    优化 首页 页面，去除无用控件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,58 +1,29 @@
 <template>
-  <div v-loading.fullscreen="loading" element-loading-text="拼命加载中" style="text-align: center">
-    <h1>HOME</h1>
-    <select v-model="selectData">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-      <option>6</option>
-      <option>7</option>
-    </select>
-    <button @click="loadingChange"> loading </button>
-    <DxDateBox
-      v-model:value="valueDate"
-      type="datetime"
-      display-format="yyyy-MM-dd HH:mm:ss"
-      :show-clear-button="true"
-    >
-    </DxDateBox>
-    <h2>{{ valueDate }}</h2>
+  <div style="width: 100%; text-align: center">
+    <h1>欢迎来到ODS平台</h1>
+    <h1>您的时间很宝贵</h1>
+    <h1>现在的时间是: {{ now }}</h1>
   </div>
 </template>
 
 <script lang="ts">
   import { defineComponent, ref } from 'vue';
-  import DxDateBox from 'devextreme-vue/date-box';
-  import moment from 'moment';
 
   export default defineComponent({
     name: 'Home',
     components: {
-      DxDateBox,
+      //
     },
     setup() {
-      const valueDate = ref(moment().format('YYYY-MM-DD HH:mm:ss').toString());
+      const now = ref(new Date().toLocaleString());
+
+      setInterval(() => {
+        now.value = new Date().toLocaleString();
+      }, 1000);
 
       return {
-        valueDate,
+        now,
       };
-    },
-    data: () => {
-      return {
-        selectData: '',
-        loading: false,
-      };
-    },
-
-    methods: {
-      loadingChange() {
-        this.loading = true;
-        setTimeout(() => {
-          this.loading = false;
-        }, 5000);
-      },
     },
   });
 </script>
