0e51c4283c1780126967d2ebd821dc8c5861de07
柯桂鑫 <1258056704@qq.com>
2021-08-30 16:47:41 +0800

    快递平台iframe迁移

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="width: 100%; text-align: center; background: #fff">
+  <div style="width: 100%; height: 100%; text-align: center; background: #fff">
     <img style="height: 400px" :src="pic" />
     <h2>披星戴月开发中, 敬请期待!</h2>
   </div>
