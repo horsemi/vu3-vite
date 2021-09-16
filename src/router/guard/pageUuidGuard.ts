0eb3cbb58bca03cb13345ce89515bfc1a9a4cff1
柯桂鑫 <1258056704@qq.com>
2021-09-16 09:45:24 +0800

    库存中心添加uuid白名单

--- src/router/guard/pageUuidGuard.ts
+++ src/router/guard/pageUuidGuard.ts
@@ -5,6 +5,7 @@ import { useAppStoreWidthOut } from '/@/store/modules/app';
 import { getUuid } from '/@/utils/uuid';
 import { isUuidRoute } from '/@/router/helper/routeHelper';
 import expressPlatform from '/@/router/routes/modules/express-platform';
+import stockCenter from '/@/router/routes/modules/stock-center';
 import strategyConfiguration from '/@/router/routes/modules/strategy-configuration';
 
 // 白名单, 不需要uuid的页面
@@ -16,8 +17,10 @@ export function createPageUuidGuard(router: Router) {
     const tabRouterMode = appStore.getSystemConfig.tabRouterMode;
     const expressWhite = await isUuidRoute(expressPlatform);
     const strategyWhite = await isUuidRoute(strategyConfiguration);
+    const stockWhite = await isUuidRoute(stockCenter);
     unIdPageList.push(...expressWhite);
     unIdPageList.push(...strategyWhite);
+    unIdPageList.push(...stockWhite);
     if (tabRouterMode === TabRouterModeEnum.MULTIPLE) {
       if (to.query && to.query.uuid) {
         next();
