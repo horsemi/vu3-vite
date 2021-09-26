374f9cb6fc14a1f9cefbc65c53fd4c5154273dd1
FE <luwenzhuo@linshimuye.com>
2021-09-26 14:21:32 +0800

    取消 组件多页面功能

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -2,11 +2,12 @@ import router from '/@/router';
 
 import { createPermissionGuard } from './permissionGuard';
 
-import { createPageUuidGuard } from './pageUuidGuard';
+// import { createPageUuidGuard } from './pageUuidGuard';
 
 import { createPageGuard } from './pageGuard';
 
-createPageUuidGuard(router);
+// TODO 单组件多页面模式存在性能问题，暂时关闭该功能
+// createPageUuidGuard(router);
 
 createPermissionGuard(router);
 
