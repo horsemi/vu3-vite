4f30527c9423e36694a411a4b2c943dbe1aace88
FE <luwenzhuo@linshimuye.com>
2021-09-26 14:23:06 +0800

    dashboard页面 添加组件测试代码

--- src/views/dashboard/analysis/index.vue
+++ src/views/dashboard/analysis/index.vue
@@ -1,133 +1,87 @@
 <template>
   <div>
-    <h1>欢迎来到ODS平台</h1>
-    <div>
-      <DxDropDownBox
-        v-model:value="gridValue"
-        v-model:opened="isGridBoxOpened"
-        :accept-custom-value="true"
-        placeholder="请选择"
-        :open-on-field-click="false"
-        :defer-rendering="false"
-        :display-expr="gridBoxDisplayExpr"
-        value-expr="Id"
-        width="250"
-        :on-initialized="onDropDownBoxInitialized"
-        @focus-in="onFocusIn"
-        @input="onInput"
-      >
-        <template #content>
-          <DxDataGrid
-            v-model:selected-row-keys="gridValue"
-            :columns="gridColumns"
-            :data-source="gridDataSource"
-            :hover-state-enabled="true"
-            height="100%"
-            :on-initialized="onInitialized"
-            @selection-changed="onSelectionChanged"
-          >
-            <DxSelection mode="single" />
-            <DxPaging :enabled="true" :page-size="10" />
-            <DxScrolling mode="virtual" />
-          </DxDataGrid>
-        </template>
-      </DxDropDownBox>
-    </div>
-    <div>
-      gridValue:
-      {{ gridValue }}
-    </div>
+    <DynamicSelect
+      v-model:value="value"
+      v-model:paramKey="requirement"
+      v-model:operation="operator"
+      v-model:paramDataType="type"
+      v-model:paramOperations="operatorList"
+      v-model:paramDatatypekeies="datatypekeies"
+      v-model:paramRelationKey="relationKey"
+      :param-list="columns"
+    ></DynamicSelect>
   </div>
 </template>
 
 <script lang="ts">
+  import type { IColumnItem } from '/@/model/types';
+
   import { defineComponent, ref } from 'vue';
-  import { useDebounceFn } from '@vueuse/core';
 
-  import DxDropDownBox from 'devextreme-vue/drop-down-box';
-  import { DxDataGrid, DxSelection, DxPaging, DxScrolling } from 'devextreme-vue/data-grid';
-
-  import { getOdsListUrlByCode, getDataSource } from '/@/api/ods/common';
+  import DynamicSelect from '/@/components/DynamicSelect/index.vue';
 
   export default defineComponent({
     name: 'Analysis',
     components: {
-      DxDropDownBox,
-      DxDataGrid,
-      DxSelection,
-      DxPaging,
-      DxScrolling,
+      DynamicSelect,
     },
     setup() {
-      const dropDownBox = ref();
-      const dataGrid = ref();
-      const gridValue = ref();
-      const isGridBoxOpened = ref(false);
-      const gridColumns = ['BillCode', 'BillTypeCode'];
-
-      const dataSource = getDataSource(
+      const value = ref(undefined);
+      const requirement = ref('foundationData');
+      const operator = ref('=');
+      const type = ref('string');
+      const operatorList = ref([
         {
-          select: ['Id', 'BillCode', 'BillTypeCode'],
+          key: '=',
+          value: '=',
         },
         {
-          url: getOdsListUrlByCode('shipping-order'),
-          version: 4,
-        }
-      );
-
-      const gridDataSource = dataSource;
-      const debounceFn = useDebounceFn(searchData, 1000);
-
-      function onInitialized(e) {
-        dataGrid.value = e.component;
-      }
-
-      function onDropDownBoxInitialized(e) {
-        dropDownBox.value = e.component;
-      }
-
-      function gridBoxDisplayExpr(item) {
-        if (item.BillCode) {
-          return `${item.BillCode}`;
-        } else {
-          return '';
-        }
-      }
-
-      function onFocusIn() {
-        isGridBoxOpened.value = true;
-      }
-
-      function onSelectionChanged() {
-        isGridBoxOpened.value = false;
-      }
-
-      function onInput(e) {
-        const value = e.event.target.value;
-        debounceFn(value);
-      }
-
-      function searchData(value: string) {
-        if (typeof value === 'string') {
-          gridDataSource.filter(['BillCode', 'contains', value]);
-          gridDataSource.load();
-        } else if (!value) {
-          dataGrid.value.clearFilter();
-        }
-      }
+          key: '<',
+          value: '<',
+        },
+      ]);
+      const datatypekeies = ref('');
+      const relationKey = ref('');
+      const columns: IColumnItem[] = [
+        {
+          key: 'id',
+          caption: 'id',
+          type: 'int32',
+        },
+        {
+          key: 'name',
+          caption: 'name',
+          type: 'string',
+        },
+        {
+          key: 'number',
+          caption: 'number',
+          type: 'decimal',
+        },
+        {
+          key: 'date',
+          caption: 'date',
+          type: 'datetime',
+        },
+        {
+          key: 'foundationData',
+          caption: '基础资料',
+          type: 'string',
+          datatypekeies: 'bill-types',
+          relationKey: 'BillTypeCode',
+          expand: 'bill-types',
+        },
+      ];
 
       return {
-        gridDataSource,
-        gridColumns,
-        dropDownBox,
-        isGridBoxOpened,
-        gridValue,
-        onInitialized,
-        onDropDownBoxInitialized,
-        gridBoxDisplayExpr,
-        onSelectionChanged,
-        onFocusIn,
-        onInput,
+        value,
+        requirement,
+        operator,
+        type,
+        operatorList,
+        datatypekeies,
+        relationKey,
+        columns,
       };
     },
   });
