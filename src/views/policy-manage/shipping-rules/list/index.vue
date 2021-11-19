b26dbfc83406bcab44909c4099aa302c0979c843
FE <luwenzhuo@linshimuye.com>
2021-11-19 11:28:35 +0800

    发运 导入成功后添加提示

--- src/views/policy-manage/shipping-rules/list/index.vue
+++ src/views/policy-manage/shipping-rules/list/index.vue
@@ -199,7 +199,10 @@
         const files = (ev.target as HTMLInputElement).files;
         let postFiles = Array.prototype.slice.call(files);
 
-        ShippingRulesApi.onShippingRulesImport(postFiles[0]);
+        ShippingRulesApi.onShippingRulesImport(postFiles[0]).then(() => {
+          useMessage('导入成功', 'success');
+          onRefresh();
+        });
       };
 
       const onDeleteClickThrottleFn = useThrottleFn(onDeleteClick, DEFAULT_THROTTLE_TIME);
